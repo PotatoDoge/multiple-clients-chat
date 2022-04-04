f7190f7690e4aa7468a6949cbb489bb3154c13e8
bprado <bastian.prado.v@gmail.com>
2022-04-03 21:49:38 -0700

    - Client side file created.

---
+++ src/MainClient.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MainClient {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the username for the group chat: ");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost",1234);
+        Client client = new Client(socket,username);
+        client.listenForMessage();
+        client.sendMessage();
+    }
+}
--- src/MainServer.java
+++ src/MainServer.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 
-public class Main {
+public class MainServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(1234);
         Server server = new Server(serverSocket);
