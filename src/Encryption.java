dcde00d4dcd60950afbf082830501c1a13794d05
bprado <bastian.prado.v@gmail.com>
2022-04-14 16:21:21 -0700

    - Encryption and decryption feature completed.

--- src/Encryption.java
+++ src/Encryption.java
@@ -11,14 +11,13 @@ public class Encryption {
 
     public Encryption(){}
 
-    // limite superior ascii 1114111
     public static String encrypt(String input, String key){
         StringBuilder cyphered = new StringBuilder();
         long newValue;
         int s = 1;
         for(int i = 0; i< input.length(); i++){
-            newValue = ((long) input.charAt(i) + ((long) key.charAt(i%key.length()) - 32));
-            newValue = newValue > 65535 ? 32 + (65535 - newValue) : newValue;
+            newValue = ((long) input.charAt(i) + ((long) key.charAt(i%key.length()) - 32)*s);
+            newValue = newValue > 65534 ? 32 + (newValue - 65535) : newValue;
             newValue = newValue < 32 ? 65535 - (32 - newValue) : newValue;
             cyphered.append((char) newValue);
             s=s*-1;
@@ -28,12 +27,12 @@ public class Encryption {
 
     public static String decrypt(String input, String key){
         StringBuilder cyphered = new StringBuilder();
-        int newValue;
+        long newValue;
         int s = -1;
         for(int i = 0; i< input.length(); i++){
-            newValue = ((int) input.charAt(i) - ((int) key.charAt(i%key.length()) - 32));
-            //newValue = newValue > 65535 ? 32 + (65535 - newValue) : newValue;
-            //newValue = newValue < 32 ? 65535 - (32 - newValue) : newValue;
+            newValue = ((long) input.charAt(i) + ((long) key.charAt(i%key.length()) - 32)*s);
+            newValue = newValue > 65534 ? 32 + (newValue - 65535) : newValue;
+            newValue = newValue < 32 ? 65535 - (32 - newValue) : newValue;
             cyphered.append((char) newValue);
             s = s*-1;
         }
@@ -44,7 +43,11 @@ public class Encryption {
         StringBuilder k = new StringBuilder();
         Random r = new Random();
         int ranLength = r.nextInt((3)) + 6; // [6,9]
-        for (int i = 0; i < ranLength; i++) { k.append(alphabet.get((int) (Math.random() * 100 % 26))); }
+        for (int i = 0; i < ranLength; i++) {
+            long ranChar = r.nextInt(30000) + 33; // [33,65502]
+            //k.append(alphabet.get((int) (Math.random() * 100 % 26)));
+            k.append((char) ranChar);
+        }
         return k.toString();
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,16 @@
 public class Test {
     public static void main(String[] args) {
-        String key = "qlk";
+        String key = "埼䚃档媩\u0C8D㮸勠䣲";
         //String text = "The quick brown fox jumps over 13 lazy dogs.";
         char x = (char) 65535;
         System.out.println(x);
-        String text = "The 1 (one) quick brown fox; jumps, over the lazy dog!!!11ñЛ";
+        String text = "The 1 (one) quick brown fox; jumps, over the lazy dog!!13098r3-490wionsfsf54/sef´``+``çf!11ñЛ";
+        //String text = "asljdnawjdn84y98qdaad.w-d adwwadawdawdlmwadaw+d``ad0w9ea d.a-d.wadj ";
         System.out.println("Original: " + text);
         String e = Encryption.encrypt(text,key);
         System.out.println("Encriptado: " + e);
         String d = Encryption.decrypt(e,key);
         System.out.println("Desencriptado: " + d);
+        System.out.println(text.equals(d));
     }
 }
