c4ff150110b51c05442c4cb0521cb42bb14e257f
Carlos M <carlosedm@protonmail.com>
2022-04-14 17:51:25 -0600

    switched ternaries to if-elseif, formatting

--- src/Encryption.java
+++ src/Encryption.java
@@ -2,43 +2,50 @@ import java.util.*;
 
 public class Encryption {
 
-    public Encryption(){}
+    public Encryption() {
+    }
 
-    public static String encrypt(String input, String key){
+    public static String encrypt(String input, String key) {
         StringBuilder cyphered = new StringBuilder();
         long newValue;
         int s = 1;
-        for(int i = 0; i< input.length(); i++){
-            newValue = ((long) input.charAt(i) + ((long) key.charAt(i%key.length()) - 32)*s);
-            newValue = newValue > 65534 ? 32 + (newValue - 65535) : newValue;
-            newValue = newValue < 32 ? 65535 - (32 - newValue) : newValue;
+        for (int i = 0; i < input.length(); i++) {
+            newValue = ((long) input.charAt(i) + ((long) key.charAt(i % key.length()) - 32) * s);
+            if (newValue > 65534) {
+                newValue = 32 + (newValue - 65535);
+            } else if (newValue < 32) {
+                newValue = 65535 - (32 - newValue);
+            }
             cyphered.append((char) newValue);
-            s=s*-1;
+            s = s * -1;
         }
         return cyphered.toString();
     }
 
-    public static String decrypt(String input, String key){
+    public static String decrypt(String input, String key) {
         StringBuilder cyphered = new StringBuilder();
         long newValue;
         int s = -1;
-        for(int i = 0; i< input.length(); i++){
-            newValue = ((long) input.charAt(i) + ((long) key.charAt(i%key.length()) - 32)*s);
-            newValue = newValue > 65534 ? 32 + (newValue - 65535) : newValue;
-            newValue = newValue < 32 ? 65535 - (32 - newValue) : newValue;
+        for (int i = 0; i < input.length(); i++) {
+            newValue = ((long) input.charAt(i) + ((long) key.charAt(i % key.length()) - 32) * s);
+            if (newValue > 65534) {
+                newValue = 32 + (newValue - 65535);
+            } else if (newValue < 32) {
+                newValue = 65535 - (32 - newValue);
+            }
             cyphered.append((char) newValue);
-            s = s*-1;
+            s = s * -1;
         }
         return cyphered.toString();
     }
 
-    public static String setKey(){
+    public static String setKey() {
         StringBuilder k = new StringBuilder();
         Random r = new Random();
         int ranLength = r.nextInt((3)) + 6; // [6,9]
         for (int i = 0; i < ranLength; i++) {
             long ranChar = r.nextInt(30000) + 33; // [33,65502]
-            //k.append(alphabet.get((int) (Math.random() * 100 % 26)));
+            // k.append(alphabet.get((int) (Math.random() * 100 % 26)));
             k.append((char) ranChar);
         }
         return k.toString();
